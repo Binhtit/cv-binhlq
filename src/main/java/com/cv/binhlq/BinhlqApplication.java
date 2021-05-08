2f48a4a1b8fae8f625a6e5234190642f430dd8b4
binhtit <devbinh@gmail.com>
2021-05-08 21:29:26 +0700

    Create project

---
+++ src/main/java/com/cv/binhlq/BinhlqApplication.java
@@ -0,0 +1,13 @@
+package com.cv.binhlq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinhlqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinhlqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cv/binhlq/BinhlqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv.binhlq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinhlqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
